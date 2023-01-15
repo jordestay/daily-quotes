ca05eef2c09250e79cc45b9a7f0d5fd9075126c3
jordestay <jordestay@gmail.com>
2023-01-14 22:18:58 -0700

    updated outDir

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/daily-quotes/",
-  root: '',
+  root: './',
   plugins: [react()],
   build: {
-    outDir: './'
+    outDir: '/docs'
   },
-  
+
 })

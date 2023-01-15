b0de3dddb6aac37b789b586f8a95fbfc03460789
jordestay <jordestay@gmail.com>
2023-01-14 21:59:01 -0700

    homepage basics

--- src/App.tsx
+++ src/App.tsx
@@ -7,25 +7,42 @@ function App() {
 
   return (
     <div className="App">
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <h1>Vite + React</h1>
+      </div> */}
+      <h1>Daily Quote</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <form
+  onSubmit={(e: React.SyntheticEvent) => {
+    e.preventDefault();
+    const target = e.target as typeof e.target & {
+      searchTerm: { value: string };
+    };
+    const searchTerm = target.searchTerm.value; // typechecks!
+    // etc...
+  }}
+>
+  <div>
+    <label>
+      Author: 
+      <input type="text" name="searchTerm" />
+    </label>
+  </div>
+  </form>
+        {/* <button onClick={() => setCount((count) => count + 1)}> */}
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+        <button>
+          Search
+        </button>
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        [Random Generated Quote Here]
       </p>
     </div>
   )

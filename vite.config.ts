d4dca74d668fe1be3e3dfe1ce98188a235a6c316
jordestay <jordestay@gmail.com>
2023-01-18 12:42:31 -0700

    updated base folder

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/daily-quotes/",
+  base: "./",
   root: './',
   plugins: [react()],
   build: {

9f1e42af4f58d4e5261e137bd79eaf2e8548f2b8
jordestay <jordestay@gmail.com>
2023-01-23 13:12:54 -0700

    updated build folder

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
-  root: './',
   plugins: [react()],
-  build: {
-    outDir: '/docs'
-  },
-
+  build: {outDir: "docs"}
 })
